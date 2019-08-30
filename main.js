16a32dc71408308fe2f94f64d01fe733ee7621d4
Laura Collins <laura.e.collins1@gmail.com>
2019-08-29 19:27:38 -0500

    adding base javascript

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log('Pet Adoption');
+const pets = [
+    {
+        image: './images/cat1.jpg',
+        name: 'Chester',
+        color: 'Flame Point Siamese',
+        specialSkill: '3am Zoomies',
+        type: 'Cat'
+    },
+    {
+        image: './images/cat2.jpg',
+        name: 'Stephanie',
+        color: 'Blue Point Siamese',
+        specialSkill: 'Rearely vomits',
+        type: 'Cat'
+    },
+    {
+        image: './images/dog1.jpg',
+        name: 'Hatchiko',
+        color: 'Buff',
+        specialSkill: 'Waits indefinitely',
+        type: 'Dog'
+    },
+    {
+        image: './images/dog2.jpg',
+        name: 'Todd',
+        color: 'Red',
+        specialSkill: "Doesn't know what to say, but when he does he says 'Yippie yip yip yip'",
+        type: 'Dog'
+    },
+    {
+        image: './images/dino1.jpg',
+        name: 'Ducky',
+        color: 'Brownish with more brown',
+        specialSkill: 'Eats tree stars, yep yep!',
+        type: 'Dino'
+    },
+    {
+        image: './images/dino2.jpg',
+        name: 'Susan',
+        color: 'Custard',
+        specialSkill: '3am Zoomies',
+        type: 'Cat'
+    },
+]
